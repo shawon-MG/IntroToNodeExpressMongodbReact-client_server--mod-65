7b44aacd1656c54f047315f136b1a448858f9378
shawon-MG <work.shawomg@gmail.com>
2022-04-26 22:48:54 +0600

    Frontend-->react and Backend-->Node-Express connected

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
 const AddUser = () => {
+
+    const handleAddUser = (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = { name, email };
+        // console.log(user);
+
+        // Sending data to the server from here :
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('Success : ', data);
+            })
+    };
+
     return (
         <div>
             <h1> Adding Users</h1>
+
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" id="" placeholder='Name' required />
+                <br />
+                <input type="email" name="email" id="" placeholder='Add Email Address' required />
+                <br /> <br />
+                <input type="submit" value="Add User" />
+            </form>
         </div>
     );
 };

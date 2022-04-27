7e99de053ea30f6fd10a71c165cd88799c567f50
shawon-MG <work.shawomg@gmail.com>
2022-04-27 13:43:26 +0600

    Showing the backend data to UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+
     return (
         <div>
-            <h1> Home </h1>
+            <h1> Users: {users.length} </h1>
+            <ul>
+                {
+                    users.map(user =>
+                        <div key={user._id}>
+                            <li> Name : {user.name}</li>
+                            <li> Email : {user.email}</li>
+                            <button>Delete</button>
+                            <br /> <br />
+                        </div>
+                    )
+                }
+            </ul>
         </div>
     );
 };

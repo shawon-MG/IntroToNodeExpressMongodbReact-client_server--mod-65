bd4ecfd855e1c5cf3c052b8055d5bf3b6cdadb1a
shawon-MG <work.shawomg@gmail.com>
2022-04-27 17:04:40 +0600

    delete a data from frontend, backend and database

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,16 +9,35 @@ const Home = () => {
             .then(data => setUsers(data))
     }, []);
 
+    const handleDeleteUser = (_id) => {
+        const proceed = window.confirm('Are you sure you want to delete?');  /* delete hobar age ekta alert massage debe */
+        if (proceed) {
+            console.log('Delete', _id);
+            const url = `http://localhost:5000/user/${_id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) { /* check browser console to understand better why-->.deleteCount*/
+                        console.log('Deleted : ', data);
+                        const remainingData = users.filter(user => user._id !== _id);  /* means, deleted _id ta ke filter kore baki remaining _id guloke remaining variable e rakhbe...NOTE: erpor ekate users e set kore dite hobe  */
+                        setUsers(remainingData);
+                    }
+                })
+        };
+    };
+
     return (
         <div>
-            <h1> Users: {users.length} </h1>
+            <h1> Users Number : {users.length} </h1>
             <ul>
                 {
                     users.map(user =>
                         <div key={user._id}>
                             <li> Name : {user.name}</li>
                             <li> Email : {user.email}</li>
-                            <button>Delete</button>
+                            <button onClick={() => handleDeleteUser(user._id)}>Delete</button>
                             <br /> <br />
                         </div>
                     )
